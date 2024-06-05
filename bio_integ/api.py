347f227dfbd24118043da59150a40d9541058338
rayen <rayenlabdouni@gmail.com>
2024-06-05 11:02:09 +0400

    disable shift type update

--- bio_integ/api.py
+++ bio_integ/api.py
@@ -50,10 +50,10 @@ def execute():
 				time = filtered_checkin[c]['punch_time']
 				location = filtered_checkin[c]['terminal_alias']
 				create_checkin(employee,time,location,punch_dict[filtered_checkin[c]['punch_state']])
-				shift_list = frappe.get_all('Shift Type', 'name', {'enable_auto_attendance':'1'}, as_list=True)
-				for row in shift_list:
-					frappe.set_value('Shift Type', row[0], 'last_sync_of_checkin', now_datetime())
-					frappe.db.commit()
+				# shift_list = frappe.get_all('Shift Type', 'name', {'enable_auto_attendance':'1'}, as_list=True)
+				# for row in shift_list:
+				# 	frappe.set_value('Shift Type', row[0], 'last_sync_of_checkin', now_datetime())
+				# 	frappe.db.commit()
 
 	# return data
 
