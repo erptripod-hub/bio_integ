391bf11388f8e6763ee31a7fb75ee505eac38f09
khadija <diju19599@gmail.com>
2025-12-11 15:34:04 +0400

    code cleanup and update, optimized working code

--- bio_integ/api.py
+++ bio_integ/api.py
@@ -5,23 +5,21 @@ from frappe.utils import date_diff
 from bio_integ.token import get_token
 from frappe.utils import now_datetime
 
+# Setup logger
+logger = frappe.logger("biometric_integration", allow_site=True, file_count=5)
+
+# Constants
+MONTHLY_ROLLOVER_DAYS = 32  # Days to wait before advancing start_time by one month
 
 
-
-settings = frappe.get_doc("Biometric Settings")
-headers = {
-		"Content-Type": "application/json",
-		"Authorization": settings.key,
-}
-payload = {
-	# "page_size": settings.size,
-}
-param = f"?start_time={settings.start_time}&page_size={settings.size}"
+def get_settings():
+	"""Get Biometric Settings document"""
+	return frappe.get_doc("Biometric Settings")
 
 
 @frappe.whitelist()
 def pull_filtered_checkin(filters, param="", next=None):
-	settings = frappe.get_doc("Biometric Settings")
+	settings = get_settings()
 	doc = frappe.get_doc("Pull Checkin")
 	loaded_filters = frappe._dict(json.loads(filters))
 	if not param:
@@ -29,101 +27,145 @@ def pull_filtered_checkin(filters, param="", next=None):
 			param = "?page_size={}&start_time={}&end_time={}&emp_code={}".format(settings.size, loaded_filters.start_time,loaded_filters.to_time, loaded_filters.attendance_device_id)
 		else:
 			param = "?page_size={}&start_time={}&end_time={}".format(settings.size, loaded_filters.start_time, loaded_filters.to_time)
+
 	if next:
-		data = send_request(next)
+		url = next
 	else:
-		data = send_request(settings.url+param)
-	if data:
-		if data["data"]:
-			checkinout = data['data']
-			doc.add_comment("Comment",f"Adding {len(checkinout)} record(s)") 
-			add_employee_checkins(checkinout)
-			if data["next"]:
-				pull_filtered_checkin(filters, param=param, next=data["next"])
-	else:
-		frappe.throw(str(data))
+		url = settings.url + param
+
+	# Iterative approach to handle pagination without recursion
+	while url:
+		data = send_request(url, settings)
+		if data:
+			if data.get("data"):
+				checkinout = data['data']
+				doc.add_comment("Comment", f"Adding {len(checkinout)} record(s)")
+				add_employee_checkins(checkinout)
+				url = data.get("next")
+			else:
+				break
+		else:
+			frappe.throw(str(data))
 
 
 @frappe.whitelist()
 def execute(next=None):
-	settings = frappe.get_doc("Biometric Settings")
+	settings = get_settings()
 	if next:
-		data = send_request(next)
+		url = next
 	else:
-		data = send_request(settings.url+param)
-	if data:
-		if data["data"]:
-			checkinout = data['data']
-			filtered_checkin = [d for d in checkinout if datetime.strptime(d['punch_time'], '%Y-%m-%d %H:%M:%S') >= datetime.strptime(settings.start_time, '%Y-%m-%d %H:%M:%S')]
-			add_employee_checkins(filtered_checkin)
-			if data["next"]:
-				execute(next=data["next"])
-	else:
-		frappe.throw(str(data))
+		param = f"?start_time={settings.start_time}&page_size={settings.size}"
+		url = settings.url + param
 
-def send_request(url):
-	print(url)
-	headers = { "Content-Type": "application/json", "Authorization": settings.key,}
-	response = requests.get(url, headers=headers, params=payload, timeout=settings.timeout, verify=False)
-	return response.json()
+	# Iterative approach to handle pagination without recursion
+	while url:
+		data = send_request(url, settings)
+		if data:
+			if data.get("data"):
+				checkinout = data['data']
+				filtered_checkin = [d for d in checkinout if datetime.strptime(d['punch_time'], '%Y-%m-%d %H:%M:%S') >= datetime.strptime(settings.start_time, '%Y-%m-%d %H:%M:%S')]
+				add_employee_checkins(filtered_checkin)
+				url = data.get("next")
+			else:
+				break
+		else:
+			frappe.throw(str(data))
+
+def send_request(url, settings):
+	logger.info(f"Fetching data from: {url}")
+	headers = {
+		"Content-Type": "application/json",
+		"Authorization": settings.key,
+	}
+	payload = {}
+	try:
+		response = requests.get(url, headers=headers, params=payload, timeout=settings.timeout, verify=False)
+		response.raise_for_status()  # Raises HTTPError for bad status codes
+		return response.json()
+	except requests.exceptions.Timeout:
+		frappe.log_error("Biometric API request timeout", "Biometric Integration")
+		frappe.throw("Biometric API request timed out. Please try again later.")
+	except requests.exceptions.HTTPError as e:
+		frappe.log_error(f"Biometric API HTTP error: {str(e)}", "Biometric Integration")
+		frappe.throw(f"Biometric API returned an error: {str(e)}")
+	except requests.exceptions.RequestException as e:
+		frappe.log_error(f"Biometric API request error: {str(e)}", "Biometric Integration")
+		frappe.throw(f"Failed to connect to Biometric API: {str(e)}")
+	except ValueError as e:
+		frappe.log_error(f"Invalid JSON response from Biometric API: {str(e)}", "Biometric Integration")
+		frappe.throw("Received invalid response from Biometric API")
 
 def add_employee_checkins(filtered_checkin):
-	settings = frappe.get_doc("Biometric Settings")
-	log_type = ""
-	l = 0
-	code = []
-	for c in range(len(filtered_checkin)):
-		if not filtered_checkin[c]["emp_code"] in code:
-			code.append(filtered_checkin[c]["emp_code"])
-		if filtered_checkin[c]['punch_state'] in ["0","1","255"]:
+	settings = get_settings()
+	checkins_created = 0
+	processed_emp_codes = []
+
+	for checkin_record in filtered_checkin:
+		emp_code = checkin_record["emp_code"]
+
+		# Track unique employee codes
+		if emp_code not in processed_emp_codes:
+			processed_emp_codes.append(emp_code)
+
+		if checkin_record['punch_state'] in ["0","1","255"]:
 			punch_dict = {"0":"IN","1":"OUT","255":""}
-			employee = frappe.db.get_value("Employee",{"attendance_device_id":filtered_checkin[c]["emp_code"]},"name")
-			if not frappe.db.exists("Employee",{"attendance_device_id":filtered_checkin[c]["emp_code"]}):
-				if not frappe.db.exists("Bio logs",{"code":filtered_checkin[c]["emp_code"]}):
+			# Single query - get_value returns None if employee doesn't exist
+			employee = frappe.db.get_value("Employee", {"attendance_device_id": emp_code}, "name")
+			if not employee:
+				if not frappe.db.exists("Bio logs", {"code": emp_code}):
 					log = frappe.new_doc("Bio logs")
-					log.code = filtered_checkin[c]["emp_code"]
-					log.log = "code {} is not attached to any employee".format(filtered_checkin[c]["emp_code"])
+					log.code = emp_code
+					log.log = "code {} is not attached to any employee".format(emp_code)
 					log.save()
 			else:
-				l+= 1
-				time = filtered_checkin[c]['punch_time']
-				location = filtered_checkin[c]['terminal_alias']
+				checkins_created += 1
+				time = checkin_record['punch_time']
+				location = checkin_record['terminal_alias']
 				try:
-					create_checkin(employee,time,location,punch_dict[filtered_checkin[c]['punch_state']])
+					create_checkin(employee, time, location, punch_dict[checkin_record['punch_state']])
 				except Exception as e:
 					frappe.log_error(f"Error adding employee checkin for employee {employee}")
 	if settings.update_last_checkin:
 		shift_list = frappe.get_all('Shift Type', 'name', {'enable_auto_attendance':'1'}, as_list=True)
-		print(shift_list)
+		logger.info(f"Updating last sync time for {len(shift_list)} shift types")
 		for row in shift_list:
-			print(row[0])
+			logger.debug(f"Updating shift type: {row[0]}")
 			frappe.set_value('Shift Type', row[0], 'last_sync_of_checkin',now_datetime())
-			frappe.db.commit()
+
+	# Commit all changes at once for better performance
+	frappe.db.commit()
 
 
-def create_checkin(employee,time,location,log_type):
-	# echeck = frappe.new_doc("Employee Checkin")
-	if frappe.db.exists("Employee Checkin",{"time":time,"employee":employee}):
-		pass
-	else:
+def create_checkin(employee, time, location, log_type):
+	"""Create an Employee Checkin record if it doesn't already exist"""
+	# Check for duplicate to avoid unnecessary processing
+	if frappe.db.exists("Employee Checkin", {"time": time, "employee": employee}):
+		return
+
+	try:
 		echeck = frappe.new_doc("Employee Checkin")
 		echeck.employee = employee
 		echeck.time = time
 		echeck.log_type = log_type
 		echeck.device_id = location
-		echeck.shift = frappe.db.get_value("Employee",employee,"default_shift")
+		echeck.shift = frappe.db.get_value("Employee", employee, "default_shift")
 		echeck.save()
-		frappe.db.commit()
+	except frappe.DuplicateEntryError:
+		# Handle race condition where record was created between check and insert
+		pass
 
 
 def update_start_time():
+	"""Update start_time to next month after MONTHLY_ROLLOVER_DAYS"""
+	settings = get_settings()
 	start = datetime.strptime(settings.start_time, "%Y-%m-%d %H:%M:%S")
-	if date_diff(datetime.today(),start) ==32:
+	if date_diff(datetime.today(), start) == MONTHLY_ROLLOVER_DAYS:
 		settings.start_time = frappe.utils.add_months(start, 1)
 		settings.save()
 
 @frappe.whitelist()
 def update_key():
+	settings = get_settings()
 	settings.db_set("key", get_token())
 	return get_token()
 		
--- bio_integ/token.py
+++ bio_integ/token.py
@@ -4,19 +4,16 @@ import frappe
 from frappe.utils.password import get_decrypted_password
 
 
-
-settings = frappe.get_doc("Biometric Settings")
-
 def get_token():
 	settings = frappe.get_doc("Biometric Settings")
 	headers = {
 		"Content-Type": "application/json",
-	} 
+	}
 	data = {
 		"username": settings.username,
 		"password": get_decrypted_password("Biometric Settings","Biometric Settings","password")
 	}
-	response = requests.post(settings.token_url,data=json.dumps(data), headers=headers,verify=False)
+	response = requests.post(settings.token_url, data=json.dumps(data), headers=headers, verify=False)
 	token = json.loads(response.text)
 	return "JWT " + token['token']
 
